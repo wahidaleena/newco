df43ff7b8a4512fbb70ad32b793de61e07d15a82
Tapple Gao <tapplek@gmail.com>
2019-07-26 18:40:02 -0500

    Fix missing PIC word

--- sql.cbl
+++ sql.cbl
@@ -16,7 +16,7 @@
              01 WS-STUDENT-REC.
                 05 WS-STUDENT-ID PIC 9(4).
                 05 WS-STUDENT-NAME PIC X(25).
-                05 WS-STUDENT-ADDRESS X(50).
+                05 WS-STUDENT-ADDRESS PIC X(50).
           EXEC SQL END DECLARE SECTION
           END-EXEC.
 
